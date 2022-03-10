3f9897d54e88d8fe9d9d4df50ded13d754cc61d7
omar bailal <omarbailal45@gmail.com>
2022-03-10 23:35:46 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins',],
+
+    },
+    extend: {
+
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'light-gray': '#F3F5F9',
+      'primary-blue': '#0083F6',
+      'text-lightgray': '#9B9EA6',
+      'text-darkgray': '#333333',
+      'text-warning': '#EDA112',
+      'bg-warning': '#FCEBDE',
+      'text-success': '#0DC637',
+      'bg-success': '#E5F6DF',
+      'text-error': '#FC5058',
+      'bg-error': '#FFCCCB',
+      'logo-lightblue': '#0083F6',
+      'logo-darkblue': '#5813F3',
+      'logo-darkgray': '#2D305D',
+      'border-gray': '#D3D3D3',
+    },
+  },
+  plugins: [],
+}
